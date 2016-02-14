c195cfe42053af4cbe07de3c087f1ebf9b1a62ea
hugur@hotmail.com <gantep27>
2016-02-14 22:01:48 +0000

    AngularJsWebApiJasmineKarmaTemplate

---
+++ AngularWebApiTddTemplate/app/test.js
@@ -0,0 +1,7 @@
+﻿'use strict';
+describe("A basic Javascript multiply operation", function () {
+    it("should be correct", function () {
+        var result = 2 * 2;
+        expect(result).toEqual(4);
+    });
+});
